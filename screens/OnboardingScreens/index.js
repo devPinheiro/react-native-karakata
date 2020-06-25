ca29cc3dfe82d308592e5fa458eea0908fb9a262
devPinheiro <samuelpinheiro41@gmail.com>
2020-06-25 09:57:00 +0100

    improved

--- screens/OnboardingScreens/index.js
+++ screens/OnboardingScreens/index.js
@@ -10,12 +10,12 @@ import { Transition } from 'react-navigation-fluid-transitions';
 
 const slides = [
   {
-    title: 'Welcome to Mnassa',
+    title: 'Welcome to Karakata',
     description: 'Build and expand your network with people you trust',
     img: "https://res.cloudinary.com/appnet/image/upload/v1592601647/karakata/piggy.png"
   },
   {
-    title: 'Connect to your community',
+    title: 'Move your parcel anywhere',
     description: 'Find and attend activities within your interest',
     img: "https://res.cloudinary.com/appnet/image/upload/v1592601646/karakata/logistic.png"
   },
@@ -48,8 +48,8 @@ const renderImages = scrollX => {
             style={{
               width: SIZES.screenWidth,
               height: '80%',
-              postion: "absolute",
-              top: -50
+              // position: "absolute",
+              // top: -10
             }}
           />
           
@@ -65,14 +65,15 @@ const renderTexts = slideIndex => {
 
   return (
     <KaraView flex={false} center bottom margin={[10, 40, 5, 40]}>
-      <KaraText animated center large semibold localeKey={`welcomeText${slideIndex + 1}`}>
+      <KaraText animated center color="white" large semibold localeKey={`welcomeText${slideIndex + 1}`}>
         {slide && slide.title}
       </KaraText>
       <KaraText
         lightbold
-        animated
+        animated 
+        color="white"
         center
-        midGrey
+        
         margin={[0]}
         localeKey={`welcomeDescription${slideIndex + 1}`}
       >
@@ -124,32 +125,22 @@ const Onboarding = ({ navigation, screenProps }) => {
   return  (
     <KaraView safe>
    
-        {/* <Transition shared="topSvg">
-          <View style={styles.svgBackground}>
-            <Svg width={SIZES.screenWidthh} height={SIZES.screenHeight * 0.2} viewBox="0 0 375 340">
-              <Defs>
-                <LinearGradient x1="90.743%" y1="87.641%" x2="10.14%" y2="3.465%" id="prefix__a">
-                  <Stop stopColor="#53B4FF" offset="0%" />
-                  <Stop stopColor="#3186FF" offset="100%" />
-                </LinearGradient>
-              </Defs>
-              <Path
-              d="M.11-2H376c-.005 204.081-.005 306.134 0 306.158-95.114 82-135.593-8.28-188-16.789C98.06 266.778 51.482 346.402.11 262.41-.037 251.212-.037 163.075.11-2z"
-              fill="url(#prefix__a)"
-              fillRule="evenodd"
-              />
-            </Svg>
-          </View>
-        </Transition> */}
-   <KaraView><Svg
+
+   <KaraView>
+     
+    <Svg
       width={SIZES.screenWidth}
       height={392}
       viewBox="0 0 414 392"
       fill="none"
     
     >
-      <Path d="M0 0h414v297c-44.5 156.5-273 91.5-414 0V0z" fill="#258EF6" />
-    </Svg></KaraView>
+      <Path d="M0 0h414v297c-44.5 156.5-273 91.5-414 0V0z" fill="#258EF6" />      
+    </Svg>
+        
+      
+    </KaraView>
+      <KaraView>{renderTexts(slideIndex)}</KaraView>
       <KaraView center middle>
         {renderImages(scrollX)}
       </KaraView>
